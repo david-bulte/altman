7647758a7048586775a1508412de3b852fc85cab
unknown <David@UZLLT01105.uz.kuleuven.ac.be>
2015-08-01 11:33:39 +0200

    google oauth

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,6 +1,42 @@
-function runBlock ($log) {
+function runBlock($log) {
   'ngInject';
-  $log.debug('runBlock end');
+
+  var ref = new Firebase("https://altman.firebaseio.com");
+  var authData = ref.getAuth();
+
+  if (authData) {
+    $log.debug("User already authenticated :", authData);
+  }
+  else {
+    ref.authWithOAuthPopup("google", function (error, authData) {
+      if (error) {
+        $log.debug("Login Failed!", error);
+      }
+      else {
+        $log.debug("Authenticated successfully with payload:", authData);
+
+        ref.child("users").child(authData.uid).set({
+          provider: authData.provider,
+          name: getName(authData)
+        });
+
+      }
+    }, {
+      remember: 'default'
+    });
+  }
+
+  function getName(authData) {
+    switch (authData.provider) {
+      case 'google':
+        return authData.google.displayName;
+      case 'twitter':
+        return authData.twitter.displayName;
+      case 'facebook':
+        return authData.facebook.displayName;
+    }
+  }
+
 }
 
 export default runBlock;
